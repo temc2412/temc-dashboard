a95aa99cbb62a30e3bdd74e63b73f65620a3ee0b
ThomasMujica <temc.business@gmail.com>
2024-09-01 18:59:25 -0400

    Modificando tittle de las cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,11 +24,15 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Recogido" value={totalPaidInvoices} type="collected" />
+        <Card title="Pendiente" value={totalPendingInvoices} type="pending" />
         <Card
-          title="Total Customers"
+          title="Facturas Totales"
+          value={numberOfInvoices}
+          type="invoices"
+        />
+        <Card
+          title="Clientes Totales"
           value={numberOfCustomers}
           type="customers"
         />
