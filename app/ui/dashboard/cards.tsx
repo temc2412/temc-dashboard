7a83f00480c8a1048e48297405b6360c61e7ed68
ThomasMujica <temc.business@gmail.com>
2024-09-02 09:47:52 -0400

    Se agrego skeleton y suspense al panel de control

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +15,24 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/*<Card title="Collected" value={totalPaidInvoices} type="collected" />*/}
-      {/*<Card title="Pending" value={totalPendingInvoices} type="pending" />*/}
-      {/*<Card title="Total Invoices" value={numberOfInvoices} type="invoices" />*/}
-      {/*<Card
-        title="Total Customers"
+      <Card title="Recogido" value={totalPaidInvoices} type="collected" />
+      <Card title="Pendiente" value={totalPendingInvoices} type="pending" />
+      <Card title="Facturas Totales" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Clientes Totales"
         value={numberOfCustomers}
         type="customers"
-      />*/}
+      />
     </>
   );
 }
