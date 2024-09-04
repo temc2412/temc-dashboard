c2635882b8c3394f96dbb09ccf730116e1081ec2
ThomasMujica <temc.business@gmail.com>
2024-09-04 10:23:56 -0400

    Quitando suspense a la tabla de facturas

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -27,9 +27,7 @@ export default async function Page({
         <Search placeholder="Buscar Facturas..." />
         <CreateInvoice />
       </div>
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense>
+      <Table query={query} currentPage={currentPage} />
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
