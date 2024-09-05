cf51b718a60586a81c9b3676426bce6b0e628385
ThomasMujica <temc.business@gmail.com>
2024-09-05 11:39:26 -0400

    Mejorando creacion de facturas

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -7,6 +7,16 @@ export default async function Page() {
 
   return (
     <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
       <Form customers={customers} />
     </main>
   );
