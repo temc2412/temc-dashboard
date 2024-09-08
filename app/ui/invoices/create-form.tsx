8837e3955c06f324fbe1f225fbd7db1d2b555aa7
ThomasMujica <temc.business@gmail.com>
2024-09-08 10:11:56 -0300

    Modificando el texto de algunos botones

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -64,7 +64,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 name="amount"
                 type="number"
                 step="0.01"
-                placeholder="Enter USD amount"
+                placeholder="Ingrese cantidad en CLP"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
               />
