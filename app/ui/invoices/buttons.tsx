ef6d4c38e4dda60279e1600c1fb7087672a30a6c
ThomasMujica <temc.business@gmail.com>
2024-09-09 09:01:37 -0300

    modificando algunos estilos

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -6,7 +6,7 @@ export function CreateInvoice() {
   return (
     <Link
       href="/dashboard/invoices/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center rounded-lg bg-khaki px-4 text-sm font-medium text-davysgray transition-colors hover:bg-davysgray hover:text-khaki focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-davysgray"
     >
       <span className="hidden md:block">Crear Factura</span>{" "}
       <PlusIcon className="h-5 md:ml-4" />
@@ -18,7 +18,7 @@ export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
       href={`/dashboard/invoices/${id}/edit`}
-      className="rounded-md border p-2 hover:bg-gray-100"
+      className="rounded-md border p-2 hover:bg-davysgray hover:text-khaki"
     >
       <PencilIcon className="w-5" />
     </Link>
@@ -29,7 +29,7 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
     <form action={deleteInvoiceWithId}>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+      <button className="rounded-md border p-2 hover:bg-davysgray hover:text-khaki">
         <span className="sr-only">Eliminar</span>
         <TrashIcon className="w-5" />
       </button>
