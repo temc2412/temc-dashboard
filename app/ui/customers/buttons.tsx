29b7f9aeab27b016b498bbc2204c046d42b341d3
ThomasMujica <temc.business@gmail.com>
2024-09-09 11:30:20 -0300

    Modulo creacion de cliente

--- app/ui/customers/buttons.tsx
+++ app/ui/customers/buttons.tsx
@@ -4,7 +4,7 @@ import { PlusIcon } from "@heroicons/react/24/outline";
 export function CreateCustomers() {
   return (
     <Link
-      href="/dashboard/invoices/create"
+      href="/dashboard/customers/create"
       className="flex h-10 items-center rounded-lg bg-khaki px-4 text-sm font-medium text-davysgray transition-colors hover:bg-davysgray hover:text-khaki focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-davysgray"
     >
       <span className="hidden md:block">Agregar Cliente</span>{" "}
