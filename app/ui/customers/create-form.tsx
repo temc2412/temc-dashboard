0c7041d0a4d67fe752c6e56f75edf90baab5874e
ThomasMujica <temc.business@gmail.com>
2024-09-10 09:58:11 -0300

    perfeccionando form create user

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -3,8 +3,8 @@
 import { CustomerField } from "@/app/lib/definitions";
 import Link from "next/link";
 import {
-  CheckIcon,
-  ClockIcon,
+  EnvelopeIcon,
+  PhotoIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
@@ -50,7 +50,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 placeholder="Ingrese email"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <EnvelopeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
@@ -69,7 +69,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 placeholder="Adjuntar Imagen"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
-              <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <PhotoIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
         </div>
